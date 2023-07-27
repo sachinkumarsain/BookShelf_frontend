57c24001e91700ca56693476a19ddaee7be52111
sachin <sachinsain21082004@gmail.com>
2023-07-27 07:37:09 +0530

    book data send

--- src/snepdeel/Cart/Cart.js
+++ src/snepdeel/Cart/Cart.js
@@ -1,66 +1,49 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-// import api_key from "../API/Api";
-// import "./Book.css";
 
 function Cart() {
   const [books, setBooks] = useState([]);
-  const aagayi = []
-  const [bookData, setBooksData] = useState([])
+  const [bookData, setBooksData] = useState([]);
+  const [aaya, setAaya] = useState([]);
 
-   
+  // const api_key = "YOUR_GOOGLE_BOOKS_API_KEY";
+  const api_key = 'AIzaSyC1D8WIkBINjc6GWc63579oia1BGoNYFcc';
 
-  // const api_key = "AIzaSyC70bmYoDKsQw-cXQfH1mYdWk3sI8X2MUA"   sween api key
+  useEffect(() => {
 
+    axios.get(`https://www.googleapis.com/books/v1/volumes?q=poetry&maxResults=40&orderBy=newest&key=${api_key}`)
+      .then((result) => {
+        setBooks(result.data.items.slice(0, 6));
+      });
+  }, []);
 
-  // ujwal api key 
-  const api_key = "AIzaSyC1D8WIkBINjc6GWc63579oia1BGoNYFcc"
+  console.log(books)
 
+  useEffect(() => {
+    setBooksData(books.map((bo) => ({
+      title: bo.volumeInfo.title,
+      author: bo.volumeInfo.authors[0],
+      image: bo.volumeInfo.imageLinks?.thumbnail,
+      description: bo.volumeInfo.description,
+      publishedDate: bo.volumeInfo.publishedDate,
+      publisher: bo.volumeInfo.publisher
 
+    })));
+  }, [books]);
 
-  //  https://www.googleapis.com/books/v1/volumes?q=flowers&maxResults=40&orderBy=newest&key=AIzaSyC70bmYoDKsQw-cXQfH1mYdWk3sI8X2MUA
 
 
   useEffect(() => {
-    axios.get(`https://www.googleapis.com/books/v1/volumes?q=romance&maxResults=40&orderBy=newest&key=${api_key}`)
-      .then((result) => {
-        // console.log((result.data.items[0].volumeInfo.description.length > 300)
-        //   ? result.data.items[0].volumeInfo.description.slice(0, 300) : result.data.items[0].volumeInfo.description)
-        // console.log(result.data.items)
-        // setBooks(result.data.items.slice(0,20));
-         result.data.items.forEach(async(bo)=>{
-           aagayi.push({
-            title: bo.volumeInfo.title,
-            author: bo.volumeInfo.authors[0],
-            image: bo.volumeInfo.imageLinks?.thumbnail,
-            description: bo.volumeInfo.description, 
-          })
-        }) 
-      });
+    // Add all upcoming items from 'bookData' to the 'aaya' state
+    setAaya((aaya) => [...aaya, ...bookData]);
+    // setAaya([])
+  }, [bookData]);
 
+  console.log(aaya);
+  // setBooksData(aagayi)
 
-  }, []);
-
-
-//  useEffect(()=>{
-//   books.forEach((bo) => {
-//     setBooksData({
-//       title: bo.volumeInfo.title,
-//       author: bo.volumeInfo.authors[0],
-//       image: bo.volumeInfo.imageLinks?.thumbnail,
-//       description: bo.volumeInfo.description, 
-//     })
-
-//   })
-//  },[books])
- 
-
-  
-console.log(bookData)
-setBooksData(aagayi)
-
-  useEffect(() => { 
-    axios.post("http://localhost:8080/cart", { bookData })
+  useEffect(() => {
+    axios.post("http://localhost:8080/cart", { aaya })
       .then((result) => {
 
         if (result.data === "books available") {
@@ -70,13 +53,13 @@ setBooksData(aagayi)
           console.log("nhi aaya data")
         }
       })
-  }, [bookData])
 
-   
+  }, [aaya])
+
+
 
 
   return (
-
     <div className="book">
       <div className="products" style={{ display: "flex", flexWrap: "wrap", width: "100%", justifyContent: "center", alignItems: "center" }}>
         {books.map((book, index) => {
@@ -87,10 +70,7 @@ setBooksData(aagayi)
               <h2>{book.volumeInfo.authors[0].length > 20 ? book.volumeInfo.authors[0].slice(0, 18) : book.volumeInfo.authors[0]}</h2>
               <p>{(book.volumeInfo.title.length > 25) ? book.volumeInfo.title.slice(0, 25) : book.volumeInfo.title}</p>
 
-
-
               <div className="footers"></div>
-
             </div>
           );
         })}
