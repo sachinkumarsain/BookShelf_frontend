58a602192ec3c948d6efa4265a0968b51d3dc173
sachin <sachinsain21082004@gmail.com>
2023-07-28 11:23:25 +0530

    showSearchBook file work

--- src/snepdeel/SearchBooks/SearchBooks.js
+++ src/snepdeel/SearchBooks/SearchBooks.js
@@ -1,23 +1,27 @@
 import React from 'react'
 import { useContext } from 'react'
 import { searchContext } from '../First'
+import "./SearchBooks.css"
 
 function SearchBooks() {
     const { searchBooksdata } = useContext(searchContext)
     console.log(searchBooksdata)
     return (
         <>
-            <div className='searchBooks' style={{backgroundColor:"orange"}}>
+            <div className='searchBooks'>
                 {
                     searchBooksdata.map((book) => {
                         return <div className='searchBook'>
-                                <h1>{book.title}</h1>
-                                <img src={book.image}></img>
-                        </div>
+                            <img src={book.image}></img>
+                            <p>{book.bookType.slice(0, 1).toUpperCase() + book.bookType.slice(1)}</p>
+                            <h1>{(book.title.length > 25) ? book.title.slice(0, 30) + "..." : book.title}</h1>
+                            <h3>{`${"By - "}${book.author}`}</h3>
 
+
+
+                        </div>
                     })
                 }
-                <h1>jfbdfb</h1>
             </div>
         </>
     )
