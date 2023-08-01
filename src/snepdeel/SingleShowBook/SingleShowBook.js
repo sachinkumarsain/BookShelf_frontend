2480b38a97f193f859ab362c0bb31166e1d0d9c5
sachin <sachinsain21082004@gmail.com>
2023-08-01 14:16:53 +0530

    author

--- src/snepdeel/SingleShowBook/SingleShowBook.js
+++ src/snepdeel/SingleShowBook/SingleShowBook.js
@@ -2,7 +2,7 @@ import React, { useContext, useState } from 'react'
 // import { useNavigate } from 'react-router-dom'
 import { searchContext } from '../First'
 import "./ShowSingleBook.css"
-import { Link } from 'react-router-dom'
+// import { Link } from 'react-router-dom'
 
 function SingleShowBook() {
     const { searchBookShow } = useContext(searchContext)
@@ -29,7 +29,13 @@ function SingleShowBook() {
             </div>
             <div className='right'>
                 <h1>{`${"Title : "} ${searchBookShow.title}`}</h1>
-                <div className='other'>
+                <h2 className='other1Content'>Author <span>&</span> BookType</h2>
+                <div className='other1'> 
+                    <h4 className='author'>{`${"Author : "}${searchBookShow.author}`}</h4>
+                    <h4 className='bookType'>{`${"BookType : "}${searchBookShow.bookType}`}</h4>
+                </div>
+                <h2 className='other2Content'>publishedDate <span>&</span> publisher</h2>
+                <div className='other2'>
                     <h4 className='publishedDate'>{`${"publishedDate : "}${searchBookShow.publishedDate}`}</h4>
                     <h4 className='publisher'>{`${"publisher : "}${searchBookShow.publisher}`}</h4>
                 </div>
